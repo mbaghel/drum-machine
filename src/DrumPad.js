16a2e6130d5442fa2099792fa2ac4ed9fbabd7b2
mbaghel <michael.baghel@gmail.com>
2018-10-29 12:16:35 +0400

    Added service worker

--- src/DrumPad.js
+++ src/DrumPad.js
@@ -4,7 +4,7 @@ const DrumPad = function(props) {
   return (
     <button className="drum-pad" id={props.name} data-keycode={props.keyCode} onMouseDown={props.onClick} onMouseUp={props.onUp} onMouseLeave={props.onUp} >
       {props.letter}
-      <audio className="clip" id={props.letter} src={props.path}   />
+      <audio className="clip" id={props.letter} src={props.path} preload="auto" />
     </button>
   )
 }
